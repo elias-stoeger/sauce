585ef36eb41e0e967d377141ca2a5c459de96e03
Krixec <krixec@alpine.my.domain>
2021-12-23 00:19:25 +0100

    patching

--- herbe/config.def.h
+++ herbe/config.def.h
@@ -1,17 +1,17 @@
-static const char *background_color = "#3e3e3e";
-static const char *border_color = "#ececec";
-static const char *font_color = "#ececec";
-static const char *font_pattern = "monospace:size=10";
+static const char *background_color = "#2e3440";
+static const char *border_color = "#ebcb8b";
+static const char *font_color = "#d8dee9";
+static const char *font_pattern = "monospace:size=12";
 static const unsigned line_spacing = 5;
 static const unsigned int padding = 15;
 
 static const unsigned int width = 450;
-static const unsigned int border_size = 2;
+static const unsigned int border_size = 3;
 static const unsigned int pos_x = 30;
-static const unsigned int pos_y = 60;
+static const unsigned int pos_y = 30;
 
 enum corners { TOP_LEFT, TOP_RIGHT, BOTTOM_LEFT, BOTTOM_RIGHT };
-enum corners corner = TOP_RIGHT;
+enum corners corner = BOTTOM_RIGHT;
 
 static const unsigned int duration = 5; /* in seconds */
 
